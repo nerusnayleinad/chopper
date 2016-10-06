faea8172211bbfc6926dd108b5ccbd3f3d020eb1
Suren Danielyan <danielyansuren@gmail.com>
2016-10-06 11:54:13 +0000

    removes 127.16 to 127.32 and any usl including google.com

--- script.js
+++ script.js
@@ -6,26 +6,41 @@ var column = document.getElementsByName("radio")[1];
 var quotes = document.getElementsByName("radio")[2];
 var resume = document.getElementById("status");
 var duplicates = document.getElementById("duplicates");
-var report_details = { removed_urls: 0, removed_private_net: 0, filtered_urls: 0 };         //object that contains the summary of the operation
+var report_details = { removed_urls: 0, removed_private: 0, filtered_urls: 0 };         //object that contains the summary of the operation
 
 function report() {
     //Array of filtered URLs. No duplicates
    resume.innerHTML = "Removed " + report_details["removed_urls"] + " duplicates." + "\n" +
-                      "Removed " + report_details["removed_private_net"] + " private IPs and localhost." + "\n" +
-                      "Filtered " + report_details["filtered_urls"] + " URLs.";
+                      "Removed " + report_details["removed_private"] + " private IPs/networks." + "\n" +
+                      "Filtered " + report_details["filtered_urls"] + " URLs." + "\n"; 
 }
 
 function filter_local_machine(urls) {
     var removed_local_urls = [];
+    var second_octet;                                               //172.xxx.0.0   second_octet = xxx
     //urls.forEach(function(url, index) {
     for(var i = urls.length - 1; i >= 0; --i) {
-        if(urls[i].startsWith("127.") || urls[i].startsWith("localhost") || urls[i].startsWith("192.168.")) {
-            removed_local_urls.push(urls[i]);
-            urls.splice(i, 1);
+        if(urls[i].startsWith("172.")) {                            //172.16.0.0 to 172.31.255.255 are private networks
+            second_octet = urls[i].split(".")[1];
+            
+            if((second_octet >= 16) && (second_octet < 32)) {
+                removed_local_urls.push(urls[i]);
+                urls.splice(i, 1);
+            }
         }
+        else
+            if(urls[i].startsWith("10.") || 
+               urls[i].startsWith("127.") || 
+               urls[i].startsWith("192.168.") ||
+               urls[i].startsWith("localhost") ||
+               urls[i].includes("google.com")) {
+                   
+                removed_local_urls.push(urls[i]);
+                urls.splice(i, 1);
+            }
     }
-
-    report_details["removed_private_net"] = removed_local_urls.length;
+    
+    report_details["removed_private"] = removed_local_urls.length;
     return urls;
 }
 
